1bb0846f193ac968c91ea54e057cbabd0595c71a
ゲーリーオブライト <cathedral2998@yahoo.co.jp>
2026-04-15 16:46:17 +0900

    トップも修正確認

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,4 @@
-// @ts-check
-// astro.config.mjs
-
 import { defineConfig } from 'astro/config';
-// import node from '@astrojs/node';
 
 import cloudflare from '@astrojs/cloudflare'; // クラウドフレアでデプロイ 0415
 import preact from "@astrojs/preact"; //検索機能
@@ -10,10 +6,6 @@ import preact from "@astrojs/preact"; //検索機能
 export default defineConfig({
 integrations: [preact({ compat: true })], //検索機能
   output: 'server',
-//   adapter: node({
-//     mode: 'standalone'
-//   }),
-
   adapter: cloudflare(),// クラウドフレアでデプロイ 0415
   
     // 開発ツールバーを無効化
