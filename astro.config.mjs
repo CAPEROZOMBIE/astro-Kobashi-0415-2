d9f7bf3540f66c179a6eb35a94217a0b07c22984
ゲーリーオブライト <cathedral2998@yahoo.co.jp>
2026-04-15 16:28:52 +0900

    astro.config.mjsをクラウドフレア版に修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,19 @@
 // astro.config.mjs
 
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
-import preact from "@astrojs/preact"; //検索
+// import node from '@astrojs/node';
+
+import cloudflare from '@astrojs/cloudflare'; // クラウドフレアでデプロイ 0415
+import preact from "@astrojs/preact"; //検索機能
 
 export default defineConfig({
-integrations: [preact({ compat: true })], //検索
+integrations: [preact({ compat: true })], //検索機能
   output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  }),
+//   adapter: node({
+//     mode: 'standalone'
+//   }),
+
+  adapter: cloudflare(),// クラウドフレアでデプロイ 0415
   
     // 開発ツールバーを無効化
   devToolbar: {
