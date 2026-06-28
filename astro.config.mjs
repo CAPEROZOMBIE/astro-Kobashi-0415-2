5c5f9ffd2b564fd095ed82739106ca44b0faddf7
Kanata-Report <cathedral2998@yahoo.co.jp>
2026-06-28 12:01:26 +0900

    久し振りにアストロ小橋更新

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,20 +2,22 @@
 // astro.config.mjs
 
 import { defineConfig } from 'astro/config';
-// import node from '@astrojs/node';
+import node from '@astrojs/node';
 import react from '@astrojs/react';
 
-import cloudflare from '@astrojs/cloudflare';
+// import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
 integrations: [react()], //検索機能,ハンバーガーメニュー
   output: 'server',
-  adapter: cloudflare(),
+  adapter: node({
+    mode: 'standalone'
+  }),
   devToolbar: {// 開発ツールバーを無効化
     enabled: false,
   },
   image: {
-    domains: ["images.microcms-assets.io"], // microCMSのドメイン　// microcmsをwebpの画像に
+    domains: ["images.microcms-assets.io"], // microCMSのドメイン
   },
   
 });
